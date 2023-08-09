36ccfc7e8c7dede616b0581943d8254f8c7af904
Meliodas <navycis@me.com>
2023-08-09 19:25:26 +0200

    Update main.py

--- main.py
+++ main.py
@@ -19,6 +19,6 @@ async def edit_promo(c, m):
   original_text = m.caption
   edit_with = '\n▫️▫️▫️▫️▫️▫️▫️▫️▫️▫️▫️▫️▫️▫️▫️▫️\n<b>Auch Du bist ein Fan von @GruppenKanaele?</b>\n▫️▫️▫️▫️▫️▫️▫️▫️▫️▫️▫️▫️▫️▫️▫️▫️'
   new_message = original_text + edit_with
-  m.edit_media_caption(cid, mid, new_message, reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton(button_text, url=button_link)]])
+  m.edit_media_caption(cid, mid, new_message, reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton(button_text, url=button_link)]]))
 
 bot.run()
